fa89c217d34ac4a74dbd91220f369154c2a43f1f
d-d-f <defreitas-david@outlook.fr>
2023-08-02 21:29:38 +0200

    update scss

---
+++ index.js
@@ -0,0 +1,31 @@
+const getData = async () => {
+  try {
+    const requete = await fetch("./public/recettes.json", {
+      method: "GET",
+    });
+
+    if (requete.ok) {
+      const data = await requete.json();
+      return [...data];
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const filterAllIngredients = (recettes) => {
+  let allIngredients = [];
+  recettes.forEach((recette) => {
+    recette.ingredients.filter(
+      (ingredient) =>
+        allIngredients.includes(ingredient.ingredient.toLowerCase()) !== true &&
+        allIngredients.push(ingredient.ingredient.toLowerCase())
+    );
+  });
+};
+
+const init = async () => {
+  const getRecettes = await getData();
+  filterAllIngredients(getRecettes);
+};
+init();
