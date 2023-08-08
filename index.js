48098ba000921bd5426f3e6f3f963080e6375998
D-D-f <defreitas-david@outlook.fr>
2023-08-08 15:29:59 +0200

    card ok

--- index.js
+++ index.js
@@ -71,7 +71,24 @@ const displayAllInfo = (array) => {
   filterAllUstensils(array);
 };
 
-const card = ({ name, image, description }) => {
+const createDivRecette = (parent, ingredient, quantity, unit) => {
+  const block = document.createElement("div");
+  const pIngred = document.createElement("p");
+  const pQuantity = document.createElement("p");
+  pIngred.classList.add("ingredient");
+  pQuantity.classList.add("quantity");
+  pIngred.style.margin = 0;
+  pIngred.style.padding = 0;
+  block.classList.add("col-6");
+  parent.append(block);
+  block.append(pIngred, pQuantity);
+  pIngred.innerText = ingredient;
+  pQuantity.innerText = `${quantity !== undefined ? quantity : ""}${
+    unit !== undefined ? unit : ""
+  }`;
+};
+
+const card = ({ name, image, description, ingredients }) => {
   const section = document.querySelector(
     ".section > .container > .allCard > .row"
   );
@@ -81,21 +98,34 @@ const card = ({ name, image, description }) => {
   const divBody = document.createElement("div");
   const title = document.createElement("h3");
   const h5 = document.createElement("h5");
+  const h5Ingred = document.createElement("h5");
   const p = document.createElement("p");
+  const containerRecette = document.createElement("div");
+  const rowRecette = document.createElement("div");
+  containerRecette.classList.add("container");
+  containerRecette.append(h5Ingred);
+  rowRecette.classList.add("row", "row-cols-2");
+  containerRecette.append(rowRecette);
   article.classList.add("col-4", "mb-4");
   article.append(divCard);
   divCard.classList.add("card", "p-0");
   img.classList.add("card-img-top", "h-50", "w-100");
   divBody.classList.add("card-body");
   section.appendChild(article);
-  divCard.append(img, divBody);
+  divCard.append(img, divBody, containerRecette);
   divBody.append(title, h5, p);
   title.innerText = name;
   img.setAttribute("src", `assets/photos/${image}`);
   img.setAttribute("alt", name);
   img.classList.add("position-relative");
   h5.innerText = "RECETTE";
+  h5Ingred.innerText = "INGRÉDIENTS";
   p.innerText = description;
+  p.classList.add("text-description");
+
+  ingredients.forEach((item) => {
+    createDivRecette(rowRecette, item.ingredient, item.quantity, item.unit);
+  });
 
   return article;
 };
